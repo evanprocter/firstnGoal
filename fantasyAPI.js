9196e552115752b0c599d63f774a7f12fb197ff5
Evan Procter <evanprocter@gmail.com>
2018-11-14 15:04:49 -0500

    adds api calls to fantasyAPI.js

---
+++ fantasyAPI.js
@@ -0,0 +1,34 @@
+const espnFF = require('espn-ff-api');
+const env = require('dotenv').config(); 
+const importantVariables = require('./config')();
+
+
+// console.log(config);
+// console.log(im.SWID);
+
+const cookies = {
+    espnS2: importantVariables.espnS2,
+    SWID: importantVariables.SWID,
+};
+
+
+espnFF.getBoxScore(cookies, importantVariables.league_id, 1, 10)
+      .then(leagueInfo => {
+        console.log(leagueInfo.boxscore.progames);
+      });
+
+// espnFF.getSingleTeamPlayers(cookies, importantVariables.league_id, 1, 10)
+//       .then(leagueInfo => {
+//         console.log(leagueInfo);
+//       });
+
+// espnFF.getLeagueScoreboard(cookies, importantVariables.league_id)
+//       .then(leagueInfo => {
+//         console.log(leagueInfo);
+//       });
+ 
+ 
+// espnFF.getLeagueStandings('', importantVariables.league_id)
+//     .then(standings => {
+//         console.log(standings);
+//     });
