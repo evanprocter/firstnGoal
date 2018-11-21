355cf5e89923287fd4bbbda4b8723b983283734a
Alex Rogers <alex.rogers823@gmail.com>
2018-11-21 10:19:15 -0500

    fixed front-end js

--- public/scripts/frontend.js
+++ public/scripts/frontend.js
@@ -1,7 +1,7 @@
 function populatePlayersOnTeampage() {
   const roster = document.querySelector('[data-roster]');
-  const players = // Array of players, mapped to get player and pos.
-  let addedRows = '<tr><td>'
+  // const players = // Array of players, mapped to get player and pos.
+  let addedRows = '<tr><td>';
   // then add to addedRows
 
   addedRows += '</td></tr>'
@@ -11,9 +11,10 @@ function populatePlayersOnTeampage() {
 
 function populatePlayersOnProfile() {
   const roster = document.querySelector('[data-roster]');
-  const players = // Array of players, mapped to get player and pos, health, lineup, current/projected points
+  // const players = // Array of players, mapped to get player and pos, health, lineup, current/projected points
   let addedRows = '<tr><td>'
   // then add to addedRows
+  addedRows += 'Tom Brady</td><td>QB</td><td>Good</td><td>Starting</td><td>23.5'
 
   addedRows += '</td></tr>'
   roster.appendChild(addedRows);
@@ -22,15 +23,15 @@ function populatePlayersOnProfile() {
 
 function populateRankings() {
   const rankings = document.querySelector('[data-rankings]');
-  const topTeams = //Array of top league teams, name only
-  let addedRows = '' // uses <p> and <span>
+  // const topTeams = //Array of top league teams, name only
+  let addedRows = '<p>1. <span>Dream Team</span></p>' // uses <p> and <span>
   // then iterate on addedRows
   rankings.appendChild(addedRows);
 }
 
 function viewLogo() {
   const logo = document.querySelector('[data-logo]');
-  const logoURL = //retrieve that users URL from instance
+  // const logoURL = //retrieve that users URL from instance
   logo.appendChild(`<p>${logoURL}</p>`);
 }
 
@@ -38,8 +39,8 @@ function personalizeHeader() {
   const league = document.querySelector('[data-league-name]');
   const match = document.querySelector('[data-upcoming-match]');
 
-  const leagueText = // league name from User class
-  const matchText = // next match from API
+  // const leagueText = // league name from User class
+  // const matchText = // next match from API
 
   league.textContent = leagueText;
   match.textContent = matchText;
